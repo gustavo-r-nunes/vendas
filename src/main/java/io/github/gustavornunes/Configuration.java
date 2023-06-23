25fd7601b91cff903245355e2f364bc0595ca676
gustavo-r-nunes <gustavo.r.nunes97@gmail.com>
2023-06-22 21:28:10 -0300

    aula 1

---
+++ src/main/java/io/github/gustavornunes/Configuration.java
@@ -0,0 +1,13 @@
+package io.github.gustavornunes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+    @Bean(name="applicationName")
+    public String applicationName(){
+        return "Sistema de Vendas";
+    }
+}
