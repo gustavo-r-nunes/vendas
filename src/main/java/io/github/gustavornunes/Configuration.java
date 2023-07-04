3edf924c7b329a32cb9546b2e4fbd17eb4ae2386
gustavo-r-nunes <gustavo.r.nunes97@gmail.com>
2023-07-03 22:24:42 -0300

    finalizando secao 2

--- src/main/java/io/github/gustavornunes/Configuration.java
+++ src/main/java/io/github/gustavornunes/Configuration.java
@@ -5,9 +5,4 @@ import org.springframework.context.annotation.Bean;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
-
-    @Bean(name="applicationName")
-    public String applicationName(){
-        return "Sistema de Vendas";
-    }
 }
---
+++ src/main/java/io/github/gustavornunes/model/Cliente.java
@@ -0,0 +1,13 @@
+package io.github.gustavornunes.model;
+
+
+import lombok.Setter;
+import lombok.Getter;
+
+@Getter
+@Setter
+public class Cliente {
+
+    private String nome;
+
+}
