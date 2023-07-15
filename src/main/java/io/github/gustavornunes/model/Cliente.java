fa87578b0a52a51c62f5c44a7686a2814d4b2950
gustavo-r-nunes <gustavo.r.nunes97@gmail.com>
2023-07-14 22:25:40 -0300

    entity manager

--- src/main/java/io/github/gustavornunes/model/Cliente.java
+++ src/main/java/io/github/gustavornunes/model/Cliente.java
@@ -1,12 +1,26 @@
 package io.github.gustavornunes.model;
 
 
-import lombok.Setter;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cliente")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
 
     private String nome;
 
--- src/main/java/io/github/gustavornunes/repository/ClientesRepository.java
+++ src/main/java/io/github/gustavornunes/repository/ClientesRepository.java
@@ -1,12 +1,14 @@
 package io.github.gustavornunes.repository;
 
 import io.github.gustavornunes.model.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
 
 @Repository
 public class ClientesRepository {
 
-    public void persistir (Cliente cliente) {
-
-    }
+    @Autowired
+    private EntityManager entityManager;
 }
